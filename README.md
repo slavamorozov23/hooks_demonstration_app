f360870ff116605c6b28e77828b541a04bd8cfaa
Slava Morozov <slavamorozov20052305@gmail.com>
2024-06-20 21:07:44 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hooks_demonstration_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/slavamorozov/hooks_demonstration_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.slavamorozov.hooks_demonstration_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
